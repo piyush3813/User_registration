d443c4ab55d60ff4a5439159ef60cf025b34c1c8
Piyush Mishra <piyushmishra.p@gmail.com>
2022-04-29 22:59:20 +0530

    valid first name , last name , mobile , and email format

---
+++ Regex.java
@@ -0,0 +1,58 @@
+package com.bridgelab;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Regex {
+    String fname,lname,email,mobile;
+
+    Regex(String fname,String lname, String email, String mobile){
+        this.fname=fname;
+        this.lname=lname;
+        this.email=email;
+        this.mobile=mobile;
+    }
+
+    private static final String F_NAME="^[A-Z]\\w{3,}$";
+    private static final String L_NAME="^[A-Z]\\w{3,}$";
+    private static final String MOBILE="^(91)[\\s][1-9][0-9]{9}$";
+    private static final String E_MAIL=("^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
+            + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$");
+
+    public Regex( String gfg) {
+    }
+
+
+    public static String gfg(){
+        Scanner sc =  new Scanner(System.in);
+
+        System.out.println("enter first name ");
+        String fname = sc.nextLine();
+        System.out.println(Pattern.matches(F_NAME,fname));
+
+        System.out.println("enter last name ");
+        String lname = sc.nextLine();
+        System.out.println( Pattern.matches(L_NAME,lname));
+
+        System.out.println("enter E-mail ");
+        String email = sc.nextLine();
+        System.out.println( Pattern.matches(E_MAIL,email));
+
+        System.out.println("enter Mobile number of format 91 xxxxxxxxxx ");
+        String mobile = sc.nextLine();
+        System.out.println( Pattern.matches(MOBILE,mobile));
+
+
+        return null;
+    }
+
+    public static void main(String[] args){
+        List<Regex> list = new ArrayList<Regex>();
+
+        Regex l1 = new Regex(gfg());
+
+    }
+}
